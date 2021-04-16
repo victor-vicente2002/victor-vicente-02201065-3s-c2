f2d881afba6e9f6c7f4a2bdfc4f91820d86138ae
victor-vicente2002 <victor.casimiro@bandtec.com.br>
2021-04-16 16:37:29 -0300

    Projeto Spring boot vazio

---
+++ avaliacao-continuada/src/main/java/br/com/bandtec/avaliacaocontinuada/AvaliacaoContinuadaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.avaliacaocontinuada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoContinuadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoContinuadaApplication.class, args);
+	}
+
+}
---
+++ avaliacao-continuada/src/test/java/br/com/bandtec/avaliacaocontinuada/AvaliacaoContinuadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.avaliacaocontinuada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoContinuadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
